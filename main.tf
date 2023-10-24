8d89dce16689b9953ad4d4c6d870b70744b8c4f8
jkritzen <j.kritzen@ontracon.de>
2023-10-24 17:30:21 +0200

    tt

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-variable "prefix" {
-  description = "The prefix to be used for the random name"
-  type        = string
-}
-
-variable "length" {
-  description = "The length of the random name"
-  type        = number
-}
-
 resource "random_pet" "pet" {
   prefix = var.prefix
   length = var.length
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  description = "The prefix to be used for the random name"
+  type        = string
+}
+
+variable "length" {
+  description = "The length of the random name"
+  type        = number
+}
